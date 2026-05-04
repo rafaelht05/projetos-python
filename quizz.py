bb04d5ed7701a06bfb41354454611df1038c1827
rafaelht05 <rafael.ht05@gmail.com>
2026-05-04 16:03:57 +0000

    v2

---
+++ quizz.py
@@ -0,0 +1,7 @@
+#--Quiz sobre Astronomia--
+print("Bem-vindo ao quiz, prepare-se para responder")
+
+print("1-Entre os planetas abaixo, qual não é considerado um planeta? \n A- Saturno. \n B- Plutão \n C- Mercúrio")
+input("Qual é a alternativa correta?").upper()
+if resposta == "B":
+    print("Resposta correta!✅")
